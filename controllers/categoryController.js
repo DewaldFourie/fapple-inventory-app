01c7fad973d1ee738686d86ef9e33265270a434a
Dewald <dewaldfourie0808@gmail.com>
2024-03-11 16:24:46 +0200

    added detail views and forms for creating new

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,8 @@
+const { Error } = require("mongoose");
 const Category = require("../models/category");
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
     // get details of categories and items count (in parallel)
@@ -30,18 +32,72 @@ exports.category_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific Category.
 exports.category_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Category Detail: ${req.params.id}`);
+    // Get details of category and all associated items (in parallel)
+    const [category, itemsInCategory] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({ category: req.params.id }, "title description").sort({ title: 1 }).exec()
+    ]);
+
+    if (category === null) {
+        // No results
+        const err = new Error("Category not found.");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("category_detail", {
+        title: "Category Details",
+        category: category,
+        category_items: itemsInCategory,
+    })
 });
 
 // Display Category create form on GET
-exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category create GET");
-});
+exports.category_create_get = (req, res, next) => {
+    res.render("category_form", { title: "Create Category" });
+};
 
 // handle Category create on post
-exports.category_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category create POST");
-});
+exports.category_create_post = [
+    // validate and sanitize the Title field
+    body("cat_title", "Category Title must contain at least 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+    body("cat_description")
+        .trim()
+        .isLength({ min: 10 })
+        .withMessage("Description must contain at least 10 characters"),
+
+    // Process request after validation and sanitization
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request
+        const errors = validationResult(req);
+
+        // Create a category object with escaped and trimmed data
+        const category = new Category({
+            title: req.body.cat_title,
+            description: req.body.cat_description,
+        });
+
+        if (!errors.isEmpty()) {
+            //There are errors. Render form again with sanitized values/err msgs
+            res.render("category_form", {
+                title: "Create Category",
+                category: category,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            // Data from form is valid
+
+            // save category
+            await category.save()
+            // Redirect to new Category record
+            res.redirect(category.url);
+        }
+    }),
+];
 
 // Display Category delete form on GET
 exports.category_delete_get = asyncHandler(async (req, res, next) => {
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,8 @@
 const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const Category = require("../models/category");
+
 
 //Display list of all the Items
 exports.item_list = asyncHandler(async (req, res, next) => {
@@ -13,18 +16,90 @@ exports.item_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific item.
 exports.item_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: item Detail: ${req.params.id}`);
+    // Get details of item 
+    const item = await Item.findById(req.params.id).populate("category").exec()
+
+    if (item === null) {
+        // no result
+        const err = new Error("Item not found.");
+        err.status = 404;
+        return next(err)
+    }
+
+    res.render("item_detail", {
+        title: "Item Details",
+        item: item,
+    })
 });
 
 // Display item create form on GET
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item create GET");
+    // Get all categories, which we must use to add our item
+    const allCategories = await Category.find().sort({ title: 1 }).exec();
+
+    res.render("item_form", {
+        title: "Create Item",
+        categories: allCategories,
+    });
 });
 
 // handle item create on post
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: item create POST");
-});
+exports.item_create_post = [
+    // validate and sanitize fields
+    body("item_title", "Title must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("item_category", "Category must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("item_description", "Description must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("item_price", "Price must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("item_count", "Item Count must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    // Process request after validation and sanitization
+    asyncHandler(async (req, res, next) => {
+        // Extract validation errors from request
+        const errors = validationResult(req)
+
+        // Create an Item object with escaped and trimmed data
+        const item = new Item({
+            title: req.body.item_title,
+            category: req.body.item_category,
+            description: req.body.item_description,
+            price: req.body.item_price,
+            count: req.body.item_count,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/err msgs
+
+            // Get all categories, which we must use to add our item
+            const allCategories = await Category.find().sort({ title: 1 }).exec();
+
+            res.render("item_form", {
+                title: "Create Item",
+                categories: allCategories,
+                item: item,
+                errors: errors.array(),
+            });
+        } else {
+            // data from form is valid. save item
+            await item.save()
+            res.redirect(item.url)
+        }
+    }),
+];
 
 // Display item delete form on GET
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
